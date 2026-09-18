db7981e1154a43969e539abc4e87410b46d42181
ahirsahil9999-dev <ahirsahil9999@gmail.com>
2026-09-18 16:13:23 +0530

    Delete Session 6 directory

--- Session 6/task 1.py
+++
@@ -1,9 +0,0 @@
-playlist_prices = {
-    "Top Hits": 99,
-    "Chill Vibes": 79,
-    "Workout Mix": 129,
-    "Bollywood Beats": 89,
-    "Lo-Fi Study": 69
-}
-
-print(playlist_prices)
--- Session 6/task 4.py
+++
@@ -1,6 +0,0 @@
-set1 = {"Dominos", "Pizza Hut", "McDonalds", "Subway"}
-set2 = {"Pizza Hut", "Subway", "KFC", "Burger King"}
-
-print("Union:", set1.union(set2))
-
-print("Intersection:", set1.intersection(set2))
