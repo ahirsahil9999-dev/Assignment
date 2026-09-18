843ab8678d95c1a125aa74211ba0b597aa552c82
ahirsahil9999-dev <ahirsahil9999@gmail.com>
2026-09-18 16:13:52 +0530

    Delete Session 8 directory

--- Session 8/task 4.py
+++
@@ -1,4 +0,0 @@
-songs = ['Kesariya', 'Believer', 'Shape of You', 'Blinding Lights', 'Excuses']
-
-for position, song in enumerate(songs, start=1):
-    print(position, song)
--- Session 8/task 5.py
+++
@@ -1,9 +0,0 @@
-followers= [120, 1500, 23000, 800, 45000] 
-
-for counts in followers:
-    if counts < 1000:
-        print("Mirco")
-    elif counts > 1000 and counts < 10000:
-        print("Influncer")
-    else:
-        print("Celebrity")
