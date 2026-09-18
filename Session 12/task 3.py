18f2b5b6fcd52dea96a777c0bc9ba791bb83b899
ahirsahil9999-dev <ahirsahil9999@gmail.com>
2026-09-18 16:05:23 +0530

    Delete Session 12 directory

--- Session 12/task 3.py
+++
@@ -1,5 +0,0 @@
-product = ["shop","handwase","clothes","scerum"]
-
-filpcart_product = list(filter(lambda product : product.startswith ("s"), product))
-
-print(filpcart_product)
--- Session 12/task 4.py
+++
@@ -1,7 +0,0 @@
-from functools import reduce
-
-orders = [120, 340, 560, 80]
-
-total = reduce(lambda x, y: x + y, orders)
-
-print("Total Bill:", total)
